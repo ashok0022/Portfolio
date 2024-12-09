31e7affa7f397e7c52fb6580354bb5ff253cb528
ashok0022 <anuj20105@gmail.com>
2024-12-10 00:56:22 +0530

    added loader UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from '../src/components/Header'
-import About from './components/About';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio'
-import Contact from './components/Contact'
+import "./App.css";
+import Header from "../src/components/Header";
+import About from "./components/About";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
+import Preloader from "../src/components/Pre";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <Header/>
-      <About/>
-      <Services/>
-      <Portfolio/>
-      <Contact/>
+      <Preloader load={load} />
+      {!load && (
+        <div>
+          <Header />
+          <About />
+          <Services />
+          <Portfolio />
+          <Contact />
+        </div>
+      )}
     </div>
   );
 }
